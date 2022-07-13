24e875f844447554d3432097223d69c088620c12
Takahiro Yamada <yamada@serverworks.co.jp>
2022-07-13 13:26:58 +0900

    :lipstick: API Endpoint を環境変数で変更できるようにした

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -10,11 +10,18 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+const ApiEndpoint = "CLOUD_AUTOMATOR_API_ENDPOINT"
 const ApiKeyEnvName = "CLOUD_AUTOMATOR_API_KEY"
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
+			"api_endpoint": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc(ApiEndpoint, client.ApiEndpoint),
+				Description: fmt.Sprintf("Cloud Automator API Endpoint. This can also be set via the %s environment variable.", ApiEndpoint),
+			},
 			"api_key": {
 				Type:        schema.TypeString,
 				Optional:    true,
@@ -35,15 +42,18 @@ func Provider() *schema.Provider {
 	}
 }
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	apiKey := d.Get("api_key").(string)
-
+	var c *client.Client
 	var diags diag.Diagnostics
+
+	apiKey := d.Get("api_key").(string)
 
 	if apiKey == "" {
 		return nil, diag.FromErr(errors.New("api_key must be set"))
 	}
 
-	client, _ := client.New(apiKey)
+	apiEndPoint := d.Get("api_endpoint").(string)
+	clientOption := client.WithAPIEndpoint(apiEndPoint)
 
-	return client, diags
+	c, _ = client.New(apiKey, clientOption)
+	return c, diags
 }
