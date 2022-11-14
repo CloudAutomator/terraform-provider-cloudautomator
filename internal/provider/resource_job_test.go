e764d7283721e6f6f7f5b4d0667fbd7f42bcc781
Takahiro Yamada <yamada@serverworks.co.jp>
2022-11-15 08:47:08 +0900

    AWSアカウントIDを空の状態でジョブを作成できるようにした

--- internal/provider/resource_job_test.go
+++ internal/provider/resource_job_test.go
@@ -1983,7 +1983,6 @@ func testAccCheckCloudAutomatorJobConfigCronRuleOneTime(rName string) string {
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "cron"
 	cron_rule_value {
@@ -2000,7 +1999,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigCronRuleWeekly(rName string) string {
@@ -2008,7 +2007,6 @@ func testAccCheckCloudAutomatorJobConfigCronRuleWeekly(rName string) string {
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "cron"
 	cron_rule_value {
@@ -2030,7 +2028,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigCronRuleMonthlyDayOfWeek(rName string) string {
@@ -2038,7 +2036,6 @@ func testAccCheckCloudAutomatorJobConfigCronRuleMonthlyDayOfWeek(rName string) s
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "cron"
 	cron_rule_value {
@@ -2059,7 +2056,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigWebhookRule(rName string) string {
@@ -2067,7 +2064,6 @@ func testAccCheckCloudAutomatorJobConfigWebhookRule(rName string) string {
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "webhook"
 
@@ -2077,7 +2073,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigScheduleRule(rName string) string {
@@ -2085,7 +2081,6 @@ func testAccCheckCloudAutomatorJobConfigScheduleRule(rName string) string {
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "schedule"
 	schedule_rule_value {
@@ -2099,7 +2094,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigSqsV2Rule(rName string) string {
@@ -2107,7 +2102,6 @@ func testAccCheckCloudAutomatorJobConfigSqsV2Rule(rName string) string {
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "sqs_v2"
 	sqs_v2_rule_value {
@@ -2122,7 +2116,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestSqsAwsAccountId(), acctest.TestSqsRegion(), acctest.TestSqsQueue(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestSqsAwsAccountId(), acctest.TestSqsRegion(), acctest.TestSqsQueue(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigAmazonSnsRule(rName string) string {
@@ -2130,7 +2124,6 @@ func testAccCheckCloudAutomatorJobConfigAmazonSnsRule(rName string) string {
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "amazon_sns"
 
@@ -2140,7 +2133,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigAuthorizeSecurityGroupIngressAction(rName string) string {
@@ -2464,7 +2457,6 @@ func testAccCheckCloudAutomatorJobConfigDelayAction(rName string) string {
 resource "cloudautomator_job" "test" {
 	name = "%s"
 	group_id = "%s"
-	aws_account_id = "%s"
 
 	rule_type = "webhook"
 
@@ -2474,7 +2466,7 @@ resource "cloudautomator_job" "test" {
 	}
 	completed_post_process_id = [%s]
 	failed_post_process_id = [%s]
-}`, rName, acctest.TestGroupId(), acctest.TestAwsAccountId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
+}`, rName, acctest.TestGroupId(), acctest.TestPostProcessId(), acctest.TestPostProcessId())
 }
 
 func testAccCheckCloudAutomatorJobConfigDeleteClusterAction(rName string) string {
