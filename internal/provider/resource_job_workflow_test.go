dc560148a8bbee9f794633c44ddd070b3826fac4
Takahiro Yamada <penta515@users.noreply.github.com>
2024-09-05 07:49:10 +0900

    リソースのテストをテーブル駆動テスト形式に変換した

--- internal/provider/resource_job_workflow_test.go
+++ internal/provider/resource_job_workflow_test.go
@@ -13,37 +13,85 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccCloudAutomatorJobWorkflow_Basic(t *testing.T) {
-	resourceName := "cloudautomator_job_workflow.test"
-	jobWorkflowName := fmt.Sprintf("tf-testacc-job-workflow-%s", utils.RandomString(12))
-	groupId := acctest.TestGroupId()
+func TestAccCloudAutomatorJobWorkflow(t *testing.T) {
+	cases := []struct {
+		name            string
+		jobWorkflowName string
+		configFunc      func(string, string) string
+		checks          []resource.TestCheckFunc
+	}{
+		{
+			name:            "BasicJobWorkflow",
+			jobWorkflowName: fmt.Sprintf("tf-testacc-job-workflow-%s", utils.RandomString(12)),
+			configFunc: func(jobWorkflowName string, groupId string) string {
+				return fmt.Sprintf(`
+				resource "cloudautomator_job" "first_job" {
+				  name      = "first_job"
+				  group_id  = %s
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		ProviderFactories: testAccProviderFactories,
-		CheckDestroy:      testAccCheckCloudAutomatorJobWorkflowDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckCloudAutomatorJobWorkflowConfigBasic(jobWorkflowName, groupId),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckCloudAutomatorJobWorkflowExists(testAccProviders["cloudautomator"], resourceName),
-					resource.TestCheckResourceAttr(
-						resourceName, "name", jobWorkflowName),
-					resource.TestCheckResourceAttr(
-						resourceName, "group_id", acctest.TestGroupId()),
-					resource.TestCheckResourceAttrPair(
-						resourceName, "first_job_id", "cloudautomator_job.first_job", "id"),
-					resource.TestCheckResourceAttr(
-						resourceName, "following_job_ids.#", "1"),
-					resource.TestCheckResourceAttrPair(
-						resourceName, "following_job_ids.0", "cloudautomator_job.following_job", "id"),
-				),
+				  rule_type = "webhook"
+
+				  for_workflow = true
+
+				  action_type = "no_action"
+				}
+
+				resource "cloudautomator_job" "following_job" {
+					name     = "following_job"
+					group_id = %s
+
+					for_workflow = true
+
+					rule_type = "no_rule"
+
+					action_type = "delay"
+					delay_action_value {
+					  delay_minutes = 1
+					}
+				}
+
+				resource "cloudautomator_job_workflow" "test" {
+					name              = "%s"
+					group_id          = %s
+					first_job_id      = cloudautomator_job.first_job.id
+					following_job_ids = [cloudautomator_job.following_job.id]
+				}
+				`, groupId, groupId, jobWorkflowName, groupId)
+			},
+			checks: []resource.TestCheckFunc{
+				resource.TestCheckResourceAttr("cloudautomator_job_workflow.test", "group_id", acctest.TestGroupId()),
+				resource.TestCheckResourceAttrPair("cloudautomator_job_workflow.test", "first_job_id", "cloudautomator_job.first_job", "id"),
+				resource.TestCheckResourceAttr("cloudautomator_job_workflow.test", "following_job_ids.#", "1"),
+				resource.TestCheckResourceAttrPair("cloudautomator_job_workflow.test", "following_job_ids.0", "cloudautomator_job.following_job", "id"),
 			},
 		},
-	})
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			groupId := acctest.TestGroupId()
+
+			resource.Test(t, resource.TestCase{
+				PreCheck:          func() { testAccPreCheck(t) },
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testAccCheckCloudAutomatorJobWorkflowDestroy,
+				Steps: []resource.TestStep{
+					{
+						Config: tc.configFunc(tc.jobWorkflowName, groupId),
+						Check: resource.ComposeTestCheckFunc(
+							append([]resource.TestCheckFunc{
+								testAccCheckCloudAutomatorJobWorkflowExists(testAccProviders["cloudautomator"], "cloudautomator_job_workflow.test"),
+								resource.TestCheckResourceAttr("cloudautomator_job_workflow.test", "name", tc.jobWorkflowName),
+							}, tc.checks...)...,
+						),
+					},
+				},
+			})
+		})
+	}
 }
 
-func testAccCheckCloudAutomatorJobWorkflowExists(accProvider *schema.Provider, n string) resource.TestCheckFunc {
+func testAccCheckCloudAutomatorJobWorkflowExists(_ *schema.Provider, n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		c := testAccProvider.Meta().(*client.Client)
 
@@ -87,40 +135,4 @@ func testAccCheckCloudAutomatorJobWorkflowDestroy(s *terraform.State) error {
 	}
 
 	return nil
-}
-
-func testAccCheckCloudAutomatorJobWorkflowConfigBasic(jobWorkflowName string, groupId string) string {
-	return fmt.Sprintf(`
-resource "cloudautomator_job" "first_job" {
-  name      = "first_job"
-  group_id  = %s
-
-  rule_type = "webhook"
-
-  for_workflow = true
-
-  action_type = "no_action"
-}
-
-resource "cloudautomator_job" "following_job" {
-	name     = "following_job"
-	group_id = %s
-
-	for_workflow = true
-
-	rule_type = "no_rule"
-
-	action_type = "delay"
-	delay_action_value {
-	  delay_minutes = 1
-	}
-}
-
-resource "cloudautomator_job_workflow" "test" {
-	name              = "%s"
-	group_id          = %s
-	first_job_id      = cloudautomator_job.first_job.id
-	following_job_ids = [cloudautomator_job.following_job.id]
-}
-`, groupId, groupId, jobWorkflowName, groupId)
 }
